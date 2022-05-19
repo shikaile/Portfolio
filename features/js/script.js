202476902b9c1458561baa9f64d0c9bf0543db6e
史开乐 <csurfssecure@gmail.com>
2022-05-19 12:34:55 -0400

    edit

---
+++ features/js/script.js
@@ -0,0 +1,28 @@
+var repoList = document.getElementById("projects");
+
+// `getApi` function is called when the `fetchButton` is clicked
+
+function getApi() {
+  // TODO: Insert the API url to get a list of your repos
+  var requestUrl = 'https://api.github.com/users/shikaile/repos';
+
+  fetch(requestUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      // Looping over the fetch response and inserting the URL of your repos into a list
+      for (var i = 0; i < data.length; i++) {
+        // Create a list element
+        var listItem = document.createElement('li');
+
+        // Set the text of the list element to the JSON response's .html_url property
+        listItem.textContent = data[i].name;
+
+        // Append the li element to the id associated with the ul element.
+        repoList.appendChild(listItem);
+      }
+    });
+}
+
+getApi();
